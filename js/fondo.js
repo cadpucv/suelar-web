08da6341e867ceb1d9733c83fce02064f415e0dd
Herbert Spencer <hspencer@ead.cl>
2023-01-05 12:58:53 -0300

    ajustes de layout

--- js/fondo.js
+++ js/fondo.js
@@ -4,17 +4,32 @@ let page;
 
 function setup(){
     page = document.getElementById('page');
-    fondo = createCanvas(windowWidth, page.offsetHeight);
+    fondo = createCanvas(page.offsetWidth, page.offsetHeight);
     fondo.position(0, 0);
     fondo.style('z-index', -1);
     w = [];
-    w[0] = new Wave(height * .70, 50, 25, "#6DCFF4");
-    w[1] = new Wave(height * .75, 100, 50, "#01A6DE");
-    w[2] = new Wave(height * .85, 160, 75, "#0067A1");
+    
+    /*
+    w[0] = new Wave(height * .62, 20, 10, "#B1D8E7");
+    w[1] = new Wave(height * .63, 25, 15, "#8EBFD5");
+    w[2] = new Wave(height * .65, 25, 20, "#6AA5C3");
+    w[3] = new Wave(height * .70, 40, 25, "#478CB0");
+    w[4] = new Wave(height * .75, 90, 50, "#23729E");
+    w[5] = new Wave(height * .85, 130, 75, "#00598C");
+*/
+
+    w[0] = new Wave(height * .62, 20, 10, "#8BDAF7ff");
+    w[1] = new Wave(height * .63, 25, 15, "#6FBDDCff");
+    w[2] = new Wave(height * .65, 25, 20, "#53A0C1ff");
+    w[3] = new Wave(height * .70, 40, 25, "#3882A7ff");
+    w[4] = new Wave(height * .75, 90, 50, "#1C658Cff");
+    w[5] = new Wave(height * .85, 130, 75, "#004871ff");
+
 }
 
 function draw(){
     clear();
+    // blendMode(MULTIPLY);
     stroke(255, 90);
     strokeWeight(2);
     
@@ -39,8 +54,8 @@ class Wave{
         noStroke();
         fill(this.col);
         beginShape();
-        for(let x = 0; x < width; x += res){
-            let y = yoff + this.y + sin(xoff/100 + (millis()/(this.y/50) + x)/this.per) * this.amp;
+        for(let x = -res; x < width + res; x += res){
+            let y = yoff + this.y + sin(xoff/100 + (millis()/(this.y/20) + x)/this.per) * this.amp;
             vertex(x, y);
         }
         vertex(width, height);
